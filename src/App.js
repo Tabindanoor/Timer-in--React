4798aef4d61d910df4f3fa3d1070e2be7333a92d
TabindaNoor <tabindanoor415@gmail.com>
2023-12-16 21:57:36 +0500

    changimgc

--- src/App.js
+++ src/App.js
@@ -2,10 +2,26 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const format =(time)=>{
+    const hour = format(time/60/60%24)
+    const minutes = format(time/60%60)
+    const seconds = format(time%60)
+
+    hour = hour<10 ? 0 + hour : hour;
+    minutes = minutes <10? '0' + minutes: minutes ;
+    seconds = seconds <10? '0' + seconds: seconds ;
+
+    return `${hour}:${minutes}:${seconds}`
+
+  }
   return (
     <div className="App">
       <div className="stopwatch">
-
+        <p>Timer</p>
+        <div>
+          <button>Reset</button>
+          <button></button>
+        </div>
       </div>
     </div>
   );
