509b9afcf7ddbfbdb51af010ca29b64427b1c691
TabindaNoor <tabindanoor415@gmail.com>
2023-12-17 21:13:19 +0500

    will do it today

--- src/App.js
+++ src/App.js
@@ -1,30 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { useRef } from 'react'
+import { useState } from 'react'
 
-function App() {
-  const format =(time)=>{
-    const hour = format(time/60/60%24)
-    const minutes = format(time/60%60)
-    const seconds = format(time%60)
+const App = () => {
+  const [time, setTime] = useState(0)
+  const [going, setGoing] = useState(true) 
+  const timer = useRef()
+  const format=(time)=>{
+    let hour = Math.floor(time/60/60%24);
+    let minutes = Math.floor(time/60%60);
+    let seconds = Math.floor(time%60);
 
-    hour = hour<10 ? 0 + hour : hour;
-    minutes = minutes <10? '0' + minutes: minutes ;
-    seconds = seconds <10? '0' + seconds: seconds ;
-
-    return `${hour}:${minutes}:${seconds}`
-
+    hour = hour < 10 ? "0"+hour : hour;
+    minutes = minutes < 10 ? "0"+minutes : minutes;
+    seconds = seconds < 10 ? "0"+seconds : seconds;
+    return `${hour} ":" ${minutes} ":" ${seconds}`
   }
   return (
-    <div className="App">
-      <div className="stopwatch">
-        <p>Timer</p>
+    <div>
+      <p>{format(time)}</p>
         <div>
-          <button>Reset</button>
-          <button></button>
+
         </div>
-      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
