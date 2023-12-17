250d19a49790a85e87d30c65ebfee0b51a348214
TabindaNoor <tabindanoor415@gmail.com>
2023-12-17 23:45:46 +0500

    done

--- src/App.js
+++ src/App.js
@@ -1,28 +1,52 @@
-import React from 'react'
-import { useRef } from 'react'
-import { useState } from 'react'
-
+import React, { useEffect, useRef, useState } from 'react'
+import "./App.css"
 const App = () => {
-  const [time, setTime] = useState(0)
-  const [going, setGoing] = useState(true) 
+  const [time,setTime] = useState(0)
+  const [going,setGoing] = useState(true)
   const timer = useRef()
-  const format=(time)=>{
-    let hour = Math.floor(time/60/60%24);
-    let minutes = Math.floor(time/60%60);
-    let seconds = Math.floor(time%60);
 
-    hour = hour < 10 ? "0"+hour : hour;
-    minutes = minutes < 10 ? "0"+minutes : minutes;
-    seconds = seconds < 10 ? "0"+seconds : seconds;
-    return `${hour} ":" ${minutes} ":" ${seconds}`
+  const formatTime=(time)=>{
+    let hour = Math.floor(time/60/60%24)
+    let minutes = Math.floor(time/60%60)
+    let seconds = Math.floor(time%60)
+
+    hour = hour <10 ?"0"+hour :hour ;
+    minutes = minutes <10 ?"0"+minutes :minutes ;
+    seconds = seconds <10 ?"0"+seconds :seconds ;
+    return `${hour} : ${minutes} : ${seconds} `
   }
-  return (
-    <div>
-      <p>{format(time)}</p>
-        <div>
 
+
+
+  useEffect(() => {
+    
+    if(going) {
+      timer.current = setInterval(()=>{
+        setTime(preTime => preTime + 1 )
+        },1000)
+    }
+    return () => {
+      clearInterval(timer.current);
+    }
+  }, [going])
+  
+
+  return (
+    <div className='App p-5 '>
+    <p className='text-pur text-5xl text-purple-700 font-serif'>Stowatch Timer In React 😀</p>
+        <br />
+      <p className='text-gray-500 text-6xl font-extrabold font-serif '>{formatTime(time)}</p>   
+      <br />
+      <div className='p-4 bg-gray-600 justify-between rounded-lg text-2xl flex  m-auto w-96 '>
+        <button className='text-red-600' onClick={()=>{setTime(0)}}>Restart</button>
+        <button className='text-green-500' onClick={()=>{
+          if(going){
+            clearInterval(timer.currentTime);
+          }
+          setGoing(!going);
+        }}>{going?"Stop":"Resume"}</button>
+      </div> 
         </div>
-    </div>
   )
 }
 
